26a7ee68d8631454b313b69b969dc8c05e25d314
Clint <crob2099@gmail.com>
2020-11-12 10:04:20 -0500

    Added migration for 'Superhero' table and updated database

---
+++ SuperheroCreator/SuperheroCreator/Data/Migrations/20201112145739_initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperheroCreator.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Superheroes",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false),
+                    AlterEgo = table.Column<string>(nullable: true),
+                    PrimaryAbility = table.Column<string>(nullable: true),
+                    SecondaryAbility = table.Column<string>(nullable: true),
+                    CatchPhrase = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Superheroes", x => x.Name);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Superheroes");
+        }
+    }
+}
